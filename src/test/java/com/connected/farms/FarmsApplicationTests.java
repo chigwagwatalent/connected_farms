6a6d185dd0bffc67fe7c0cbac16cee18055fbd32
TALENT <talentchigwagwa@gmail.com>
2025-11-09 21:24:32 +0200

    jj

---
+++ src/test/java/com/connected/farms/FarmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connected.farms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
